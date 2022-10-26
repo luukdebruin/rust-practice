aa5814943ddd5abb44cc3caa3471a2e6fee8d7d1
Luuk de Bruin <luuk.debruin15@gmail.com>
2022-10-26 11:21:36 +0200

    hello world

---
+++ 01 hello_world/target/debug/deps/hello_world-93bf6afec42167f4.d
@@ -0,0 +1,5 @@
+/Users/luukdebruin/Projects/rust-practice/01 hello_world/target/debug/deps/hello_world-93bf6afec42167f4: src/main.rs
+
+/Users/luukdebruin/Projects/rust-practice/01 hello_world/target/debug/deps/hello_world-93bf6afec42167f4.d: src/main.rs
+
+src/main.rs:
---
+++ 01 hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/luukdebruin/Projects/rust-practice/01\ hello_world/target/debug/hello_world: /Users/luukdebruin/Projects/rust-practice/01\ hello_world/src/main.rs
